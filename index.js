0c5a8def706704d2796189af266afe067204b966
Aman Verma <aman97104@gmail.com>
2026-03-08 16:43:29 +0530

    css file

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ import path from 'path';
 const app = express();
 
 const PORT = 3002;
+const absPath = path.resolve("view")
+const publicPath = path.resolve("Public")
+
+app.use(express.static(publicPath))
 
 app.get("/",(req,resp)=>{
-    let absPath = path.resolve('view/home.html');
-    console.log(absPath)
-    resp.sendFile(absPath)
+    resp.sendFile(absPath+"/home.html")
 })
 app.get("/login",(req,resp)=>{
-    let absPath = path.resolve('view/login.html');
-    resp.sendFile(absPath)
+    resp.sendFile(absPath+"/login.html")
+})
+app.get("/about",(req,resp)=>{
+    resp.sendFile(absPath+"/about.html")
+})
+
+app.use((req,resp)=>{
+    resp.status(404).sendFile(absPath+"/404.html");
 })
 
 app.listen(PORT);
