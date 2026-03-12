e279f3d478ead185e9a3de89e1550964a59c045a
Aman Verma <aman97104@gmail.com>
2026-03-12 15:15:01 +0530

    internal middleware

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-import express from 'express'
-
+import express from 'express';
+import path from "path"
 const app = express();
 
+const publicPath = path.resolve("view/form.html")
+
 const PORT = 3002;
-
-function checkAge(req,resp,next){
-    console.log(req.query.age)
-
-    if( !req.query.age || req.query.age<18){
-        resp.send("under age not allowed")
-    }else{
-        next()
-    }
-}
-function checkUrl(req,resp,next){
-    console.log("url check");
-    next()
-}
-// app.use(checkAge)
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
 
 app.get("/",(req,resp)=>{
-    resp.send("<h1>This is Home Page</h1")
-})
-app.get("/login",checkAge,(req,resp)=>{
-    resp.send("<h1>This is Login Page</h1")
-})
-app.get("/user",checkUrl,(req,resp)=>{
-    resp.send("<h1>This is User Page</h1")
-})
-app.get("/products",checkAge,checkUrl,(req,resp)=>{
-    resp.send("<h1>This is Products Page</h1")
+    resp.send("sup")
 })
 
-app.listen(PORT)
-console.log(`runnng at port ${PORT}`)
+app.get("/login",(req,resp)=>{
+    resp.sendFile(publicPath)
+})
+app.post("/submit",(req,resp)=>{
+    resp.send("This is submit page")
+    console.log(req.body)
+})
+app.get("/users",(req,resp)=>{
+    resp.send("This is users page")
+})
+
+app.listen(PORT);
+console.log(`running at ${PORT}`)
