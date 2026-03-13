4b8a52597adc5e6eb80d2bb5f9bff10db7341155
Aman Verma <aman97104@gmail.com>
2026-03-13 15:31:51 +0530

    url parameter

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 import express from 'express';
+import userData from "./users.json" with {type:"json"}
 
 const app = express();
 const PORT = 3002;
 
-
 app.get("/",(req,resp)=>{
-    const users = ["anil","sanam","sahil","neyaj"];
-    let data = `<ul>`;
-    for(let i=0; i<users.length; i++){
-
-        data+=`<li><a href="user/${users[i]}" >${users[i]}</a></li>`
-        console.log(users[i])
-    }
-    data+=`</ul>`
-    resp.send(data)
+    resp.send(userData)
 })
-app.get("/user/:name",(req,resp)=>{
-    console.log(req.params.name)
-    resp.send(`this is ${req.params.name} profile page`);
+app.get("/user/:id",(req,resp)=>{
+    const id = req.params.id;
+    let filterData = userData.filter((user)=>user.id==id)
+    resp.send(filterData)
+    console.log(id)
+})
+
+app.get("/username/:name",(req,resp)=>{
+    const name = req.params.name;
+    let filterData = userData.filter((user)=>user.name.toLowerCase()==name.toLowerCase());
+    resp.send(filterData);
+    console.log(name)
 })
 
 app.listen(PORT);
-console.log(`Running on ${PORT}`)
+console.log(`listening at ${PORT}`);
